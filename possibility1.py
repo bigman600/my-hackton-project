f53437bf54fba2c5d70e0707013a0b0a71f1170d
bigman600 <yvesmanishimwe24@gmail.com>
2025-09-24 22:08:27 +0200

    i added few lines of a code

--- possibility1.py
+++ possibility1.py
@@ -1,12 +1,15 @@
 # List of Rwanda's provinces
-provinces = ["Kigali", "Northern", "Southern", "Eastern", "Western"]
-nationality =( "rwanda")
+weather = "Today in Rwanda"
+print(weather)
+provinces = ("Kigali", "Northern", "Southern", "Eastern", "Western")
+nationality = ("rwanda")
+print(provinces)
 def login():
-    province =input ("\nselect your province : ")
+    province = input("\nselect your province : ")
     nationality = input("Enter your nationality :")
-    if nationality != "rwanda":
-        print("welcome to weather today! ")
-        return True
+    if nationality == "rwanda":
+        return  " welcome to " + province + " weather forecast news today"
     else:
-        print("access denied. please make sure your nationality is rwanda")
+        return ("access denied. please make sure your nationality is rwanda")
+print(login())
               
