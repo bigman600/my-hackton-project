61979fd87b69026cbcdf637aa512dd2b218f5b03
bigman600 <yvesmanishimwe24@gmail.com>
2025-09-28 22:59:36 +0200

    new added code

--- possibility1.py
+++ possibility1.py
@@ -1,36 +1,13 @@
-# weather forecast for Rwanda
 import kivy
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout   
-from kivy.uix.button import Button
-from plyer import notification
-from kivy.clock import Clock
+from kivy.uix.floatlayout import Layout
+
+class me(app):
 
 
-class weather(App):
-    def build(self):
-        layout = BoxLayout(orientation="vertical", padding=10, spacing=10)
-        layout = BoxLayout()
-        Clock.schedule_interval(self.auto_reminder, 30)  # every 30 sec (test)
-        return layout
-    def auto_reminder(self, dt):
-        self.show_notification(imvura)
-        layout = BoxLayout(orientation="vertical", padding=10, spacing=10)
-        
 
-        # button to trigger notification
-        btn = Button(text="Iteganyagihe", size_hint=(1, 0.2), font_size=24)
-        btn.bind(on_press=self.show_notification)       
-        layout.add_widget(btn)
-        return layout
-    def show_notification(self, instance):
-        notification.notify(
-            title="Weather Update",
-            message="uyu munsi hateganyijwe imvura muturere twose two mu gihugu!",
-            app_name="WeatherApp",
-            timeout=5
-        )      
+class me():
+   def build(self):
+      
+      
 
-if __name__ == "__main__":
-    weather().run() 
-
+if __name__ = ('main'):
